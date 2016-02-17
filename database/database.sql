d4f3d2727bf3e55784c68b9fb92382b73d391359
Daniel Teixeira <dansouza@gmail.com>
2016-02-17 20:15:33 -0200

    Adding first database scripts

---
+++ database/database.sql
@@ -0,0 +1,15 @@
+create table diary (
+  id BIGINT NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
+  title varchar(50) NOT NULL,
+  post varchar (2000) NOT NULL,
+  -- create_date date NOT NULL,
+  -- last_update_date date NOT NULL
+  PRIMARY KEY (id)
+);
+
+insert into diary (title, post) values ('My vacations', 'My vacations were boring');
+insert into diary (title, post) values ('My Job', 'My job is awesome!');
+insert into diary (title, post) values ('Kung Pow', 'You should be able to beat him now!');
+insert into diary (title, post) values ('Kung Pow', 'You should be able to beat him now!');
+
+SELECT id, title, post FROM diary ORDER BY id;
