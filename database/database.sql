9949e5f73d542cc77330617b604bbb8f18fd9577
Daniel Teixeira <dansouza@gmail.com>
2016-02-19 02:50:30 -0200

    Update database script

--- database/database.sql
+++ database/database.sql
@@ -6,10 +6,3 @@ create table diary (
   last_update_date	timestamp NOT NULL,
   PRIMARY KEY (id)
 );
-
-insert into diary (title, post) values ('My vacations', 'My vacations were boring');
-insert into diary (title, post) values ('My Job', 'My job is awesome!');
-insert into diary (title, post) values ('Kung Pow', 'You should be able to beat him now!');
-insert into diary (title, post) values ('Kung Pow', 'You should be able to beat him now!');
-
-SELECT id, title, post FROM diary ORDER BY id;
