3531fb05cbb4b15b9a0ab8b5e6ad1a4adbeb4067
Daniel Teixeira <dansouza@gmail.com>
2016-06-08 23:22:02 -0300

    Update ibm watson classes. Remove database credentials and bluemix credentials from the code.

--- src/main/java/com/diary/service/ToneAnalyzerService.java
+++ src/main/java/com/diary/service/ToneAnalyzerService.java
@@ -11,20 +11,20 @@ import com.ibm.watson.developer_cloud.tone_analyzer.v3.model.ToneAnalysis;
 @Service
 public class ToneAnalyzerService {
 
-	private static final String USERNAME = "36edf3c1-c4c0-47fa-a67c-74060f8d3839";
-	private static final String PASSWORD = "yhrL9pYpDuxN";
+	private static final String USERNAME = "<username>";
+	private static final String PASSWORD = "<password>";
 
 	private static final String TONE_CATEGORY_EMOTION = "emotion_tone";
-	private static final String TONE_CATEGORY_WRITING = "writing_tone";
+	private static final String TONE_CATEGORY_WRITING = "language_tone";
 	private static final String TONE_SOCIAL_EMOTION = "social_tone";
 
 	public ToneInfo getToneInfo(String text) {
 
-		ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_02_11);
+		ToneAnalyzer service = new ToneAnalyzer(ToneAnalyzer.VERSION_DATE_2016_05_19);
 		service.setUsernameAndPassword(USERNAME, PASSWORD);
 
 		// Call the service and get the tone
-		ToneAnalysis tone = service.getTone(text);
+		ToneAnalysis tone = service.getTone(text, null).execute();
 
 		ToneInfo toneInfo = new ToneInfo();
 		
